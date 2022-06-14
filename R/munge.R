37499eaed01b76563367c807bb026fceb083883a
cdemkegit <craig.demke@dixie.edu>
2022-06-14 16:26:10 -0600

    changed rscript to R

--- R/munge.R
+++ R/munge.R
@@ -2,7 +2,9 @@
 library(here)
 
 # DATA IMPORT ####
-source(here::here('rscript', 'data_io_util.R'))
+source(here::here('R', 'data_io_util.R'))
 
 
 s_14 <- get_data_from_sql_file('s_14.sql', 'edify')
+
+c_17 <- get_data_from_sql_file('c_17.sql', 'edify')
