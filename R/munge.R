210573ffa9b5bada40fcb1bc78ccad30a3476a29
cdemkegit <craig.demke@dixie.edu>
2022-06-15 17:04:18 -0600

    added test sql to see if we can pull C-02 data

--- R/munge.R
+++ R/munge.R
@@ -8,3 +8,5 @@ source(here::here('R', 'data_io_util.R'))
 s_14 <- get_data_from_sql_file('s_14.sql', 'edify')
 
 c_17 <- get_data_from_sql_file('c_17.sql', 'edify')
+
+c_02 <- get_data_from_sql_file('c_02.sql', 'edify')
