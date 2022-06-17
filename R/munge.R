6a80967701633f7a7c4efe596f0d9bcc14bc1ed3
cdemkegit <craig.demke@dixie.edu>
2022-06-17 11:07:23 -0600

    added sql to test function

--- R/munge.R
+++ R/munge.R
@@ -12,3 +12,5 @@ c_17 <- get_data_from_sql_file('c_17.sql', 'edify')
 c_02 <- get_data_from_sql_file('c_02.sql', 'edify')
 
 c_10 <- get_data_from_sql_file('c_10.sql', 'edify')
+
+c_44  <- get_data_from_sql_file('c_44.sql', 'edify')
