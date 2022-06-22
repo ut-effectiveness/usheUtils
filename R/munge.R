827cf507ca046b9f5c5c7c356fb3fc2e1d5b93f6
cdemkegit <craig.demke@dixie.edu>
2022-06-22 13:39:49 -0600

    added the test sql for c_50 to the munge file

--- R/munge.R
+++ R/munge.R
@@ -16,3 +16,5 @@ c_10 <- get_data_from_sql_file('c_10.sql', 'edify')
 c_44  <- get_data_from_sql_file('c_44.sql', 'edify')
 
 c_14  <- get_data_from_sql_file('c_14.sql', 'edify')
+
+c_51  <- get_data_from_sql_file('c_51.sql', 'edify')
