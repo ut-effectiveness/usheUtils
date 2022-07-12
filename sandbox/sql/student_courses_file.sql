2147654c01e14d170c1ea856371de7b218c154df
cdemkegit <craig.demke@dixie.edu>
2022-07-12 09:54:07 -0600

    made minor change

--- sandbox/sql/student_courses_file.sql
+++ sandbox/sql/student_courses_file.sql
@@ -17,7 +17,8 @@
           b.latest_student_type_code,
           a.budget_code,
           a.course_reference_number,
-          a.course_level_id
+          a.course_level_id,
+          a.version_desc
      FROM export.student_section_version a
 LEFT JOIN export.student_version b ON b.student_id = a.student_id
       AND b.version_snapshot_id = a.version_snapshot_id
