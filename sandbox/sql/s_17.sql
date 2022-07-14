b471e64bfb6d0a1413e2a7552661188878a4cef2
cdemkegit <craig.demke@dixie.edu>
2022-07-14 16:27:25 -0600

    made changes to S-17.sql

--- sandbox/sql/s_17.sql
+++ sandbox/sql/s_17.sql
@@ -1,3 +1,13 @@
 --S-17
-SELECT DISTINCT (student_type_code)
-FROM student_term_level;
+SELECT c.term_start_date,
+       b.high_school_graduation_date,
+       a.student_type_code,
+       a.term_id
+FROM student_term_level a
+LEFT JOIN student b
+ON a.student_id = b.student_id
+LEFT JOIN term c
+ON a.term_id = c.term_id
+WHERE a.term_id = '202220'
+AND student_type_code = 'F'
+LIMIT 10;
