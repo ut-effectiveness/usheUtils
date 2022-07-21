b30193dc1e8f48091fa87f784e7a810ba838c5e0
cdemkegit <craig.demke@dixie.edu>
2022-07-21 11:52:51 -0600

    updated the building sql

--- sandbox/sql/space_inventory_buildings_file.sql
+++ sandbox/sql/space_inventory_buildings_file.sql
@@ -1,7 +1,11 @@
 -- Space Inventory Buildings
 SELECT a.building_location_code,
        a.building_location_desc,
-       a.building_auxiliary,
+       a.building_ownership_code,
+       --a.inventory_year
+       a.building_name,
+       a.building_number,
+       a.building_abbrv,
        a.building_construction_year,
        a.building_remodel_year,
        a.building_cost_replacement,
@@ -9,7 +13,5 @@ SELECT a.building_location_code,
        a.building_condition_desc,
        a.building_area_gross,
        a.building_cost_myr,
-       a.building_number,
        a.building_auxiliary
-
 FROM export.buildings a;
