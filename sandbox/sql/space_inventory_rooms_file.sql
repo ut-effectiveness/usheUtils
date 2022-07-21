e56b5c862e1b1c48da3eff221dbd682839148608
cdemkegit <craig.demke@dixie.edu>
2022-07-21 11:53:06 -0600

    updated the rooms sql

--- sandbox/sql/space_inventory_rooms_file.sql
+++ sandbox/sql/space_inventory_rooms_file.sql
@@ -1,6 +1,8 @@
 --Space Inventory Rooms
-SELECT a.rooms_id,
+SELECT a.building_id,
        a.room_number,
+       '' AS room_suffix,
+       a.room_group1_code,
        a.room_use_code,
        a.room_name,
        a.room_stations,
@@ -9,5 +11,6 @@ SELECT a.rooms_id,
        a.room_prorated,
        a.room_prorated_area,
        a.room_acitivity_date
-
 FROM export.rooms a;
+
+
