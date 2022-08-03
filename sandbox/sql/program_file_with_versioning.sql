bcf1866cd33cb39f4556ba4e39c08970931f577a
cdemkegit <craig.demke@dixie.edu>
2022-08-03 10:16:17 -0600

    updated program file SQL

--- sandbox/sql/program_file_with_versioning.sql
+++ sandbox/sql/program_file_with_versioning.sql
@@ -1,4 +1,4 @@
--- Program File Submission with versioning
+-- Program File Submission
 SELECT DISTINCT(a.major_desc),
                 a.cip_code,
                 a.ipeds_award_level_code,
@@ -6,11 +6,6 @@ SELECT DISTINCT(a.major_desc),
                 a.is_perkins,
                 'N' AS "technical_education",
                 a.required_credits,
-                '' AS "program_participation_agreement", -- may need make function if fin aid says yes otherwise hard code N
-                a.version_desc,
-                b.term_id
-FROM export.academic_programs_version a
-LEFT JOIN export.student_term_level_version b
-    ON a.version_id = b.version_id
-WHERE required_credits IS NOT NULL
-AND b.term_id >= '202220';
+                'N' AS "program_participation_agreement"
+FROM export.academic_programs a
+WHERE required_credits IS NOT NULL;
