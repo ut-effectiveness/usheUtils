0fbbb843fcf1d55a4b2d414e9c6c9f3997a8ebd4
cdemkegit <craig.demke@dixie.edu>
2022-08-23 16:22:15 -0600

    using this for copy and past to build the new Rfiles.

--- sandbox/scratch.R
+++ sandbox/scratch.R
@@ -1,40 +1,34 @@
-#' Calculate USHE Element s_50 (Degree Intent of Second Degree)
+#' Calculate USHE Element b_15 (Auxiliary Building)
 #'
 #' @details
 #'
 #' **USHE Documentation**
-#' - ELEMENT NAME: Degree Intent of Second Degree
-#' - FIELD NAME: S_DEG_INTENT2
-#' - FIELD FORMAT: Varchar, 2 Character
-#' - DEFINITION: The length/nature of the degree that the student is working toward.
-#'               S_DEG_INTENT2 with S_CURR_CIP2 represents the student’s major and ties to S_MAJOR2
+#' - ELEMENT NAME: Auxiliary Building
+#' - FIELD NAME: b_aux
+#' - FIELD FORMAT:  Numeric 1 Character.
+#' - DEFINITION: Business space or other support facilities (as distinguished from primary programs of  instruction, research, and public service, and from organized activities and intercollegiate athletics)
+#'
 #'
 #' @importFrom magrittr %>%
 #' @importFrom dplyr mutate
 #' @importFrom dplyr select
 #'
-#' @param input_df A Data Frame. Must contain the following data fields: (secondary_ipeds_award_level_code).
+#' @param input_df A Data Frame. Must contain the following data fields: (building_auxiliary).
 #' @param with_intermediates Boolean: Option to include intermediate calculated fields.
 #'
-#' @return Original data frame, with USHE data element s_50 appended. Will also return appended intermediate calculated fields, if option is set.
+#' @return Original data frame, with USHE data element b_15 appended. Will also return appended intermediate calculated fields, if option is set.
 #' @export
 #'
 #' @examples
-#' s_50()
+#' b_04()
 #'
-s_50 <- function(input_df=usheUtils::fake_student_df, with_intermediates=FALSE) {
+b_15 <- function(input_df=usheUtils::fake_student_df, with_intermediates=FALSE) {
 
   output_df <- input_df %>%
     # Calculate intermediate fields
-    mutate(s_deg_intent = secondary_ipeds_award_level_code) %>%
-    # Append USHE data element s_50
-    mutate( s_50 = s_deg_intent )
-
-  if (!with_intermediates) {
-    output_df <- output_df %>%
-      # Remove fields used for intermediate calculations
-      select( -c(s_deg_intent) )
-  }
+    mutate( b_aux = building_auxiliary ) %>%
+    # Append USHE data element b_15
+    mutate( b_15 =  b_aux )
 
   return(output_df)
 }
